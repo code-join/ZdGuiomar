03e7ba1988a601de02090b65db6e5dc96601c568
jpcp0614 <jpcp0614@gmail.com>
2023-08-25 15:25:10 -0300

    :sparkles: :lipstick: feat - CSS e App

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// CSS
+import './App.css';
 
-import { Outlet } from 'react-router-dom'
+// React Router
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-     
-      <Outlet />
-    </>
-  )
+	return <Outlet />;
 }
 
-export default App
+export default App;
